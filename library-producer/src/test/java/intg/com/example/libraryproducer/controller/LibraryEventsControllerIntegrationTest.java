1f4e39e75d8dbe8000981f2925fbeb5c40a70177
daotrongthuan1010 <conxinloime1010@gmail.com>
2024-02-25 22:46:03 +0700

    Test kafka-spring with library event

--- library-producer/src/test/java/intg/com/example/libraryproducer/controller/LibraryEventsControllerIntegrationTest.java
+++ library-producer/src/test/java/intg/com/example/libraryproducer/controller/LibraryEventsControllerIntegrationTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
-import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.TestPropertySource;
 
