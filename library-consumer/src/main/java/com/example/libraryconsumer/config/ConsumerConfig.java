5658516e6b822e26a5b97d1e550f0dcaf3dff7a9
daotrongthuan1010 <conxinloime1010@gmail.com>
2024-02-25 22:45:43 +0700

    Test kafka-spring with library event consumer

---
+++ library-consumer/src/main/java/com/example/libraryconsumer/config/ConsumerConfig.java
@@ -0,0 +1,13 @@
+package com.example.libraryconsumer.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Slf4j
+@EnableKafka
+@Configuration
+public class ConsumerConfig {
+
+}
