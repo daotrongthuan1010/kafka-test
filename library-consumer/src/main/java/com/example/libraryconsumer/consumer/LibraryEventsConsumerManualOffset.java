6700bb33a35d97d48ba2746061438db44ecbcb0a
daotrongthuan1010 <conxinloime1010@gmail.com>
2024-02-26 22:32:54 +0700

    Test kafka-spring with library event

---
+++ library-consumer/src/main/java/com/example/libraryconsumer/consumer/LibraryEventsConsumerManualOffset.java
@@ -0,0 +1,17 @@
+package com.example.libraryconsumer.consumer;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.AcknowledgingMessageListener;
+import org.springframework.kafka.support.Acknowledgment;
+
+public class LibraryEventsConsumerManualOffset implements AcknowledgingMessageListener<Integer, String> {
+
+    @Override
+    @KafkaListener(topics = {"library-events"})
+    public void onMessage(ConsumerRecord<Integer, String> data, Acknowledgment acknowledgment) {
+            acknowledgment.acknowledge();
+    }
+
+}
